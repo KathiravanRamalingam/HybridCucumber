85349adaa084dab70e0607fa5674e28afc9da96b
Kathiravan Ramalingam <kathiravan.1708010@srec.ac.in'>
2021-09-30 16:55:01 +0530

    My first commit

---
+++ HybridSeleniumCucumber/src/test/java/com/testCase/LoginTestCase.java
@@ -0,0 +1,37 @@
+package com.testCase;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.baseClass.Library;
+import com.pages.LoginPage;
+import com.seleniumReusableFunctions.SeleniumUtilities;
+
+public class LoginTestCase extends Library{
+	LoginPage lpage;
+	SeleniumUtilities sUti;
+	@BeforeTest
+    public void stratUp() {
+        launchApplication("chrome","https://opensource-demo.orangehrmlive.com/");
+       
+    }
+    @Test
+    public void login() {
+        lpage=new LoginPage(driver);
+        lpage.login("Admin", "admin123");
+       
+    }
+    @AfterClass
+    public void close() throws IOException {
+    	sUti = new SeleniumUtilities(driver);
+        sUti.to_take_screenshot("src\\test\\resources\\screenshots\\orangeHRMLogin.png");
+       
+        quit();
+       
+    }
+ 
+
+}
